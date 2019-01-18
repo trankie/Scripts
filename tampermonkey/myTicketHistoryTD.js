2ad0ab82811b8d5c3e5fe19191d3f73b883075ac
Kien Tran <kientran@SD01-IM27.tss.oregonstate.edu>
2019-01-18 12:16:40 -0800

    First initial commit for starting ticket history button on td

---
+++ tampermonkey/myTicketHistoryTD.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         My Ticket History Finder Team Dynamics(TD)
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Creating a button to get me all the tickets I have worked on in the past
+// @author       You
+// @match        https://oregonstate.teamdynamix.com/TDNext/Home/Desktop/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
