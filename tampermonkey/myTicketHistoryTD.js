ec4433f65ce8125afcaf1e880be838b565ecf944
Kien Tran <kientran@SD01-IM27.tss.oregonstate.edu>
2019-01-18 12:37:10 -0800

    Adding button to allow for functionality that will be implement later

--- tampermonkey/myTicketHistoryTD.js
+++ tampermonkey/myTicketHistoryTD.js
@@ -6,10 +6,24 @@
 // @author       You
 // @match        https://oregonstate.teamdynamix.com/TDNext/Home/Desktop/*
 // @grant        none
+// @require http://code.jquery.com/jquery-3.3.1.min.js
 // ==/UserScript==
+var $ = window.jQuery;
 
-(function() {
-    'use strict';
+function addGlobalStyle(css) {
+  var head, style;
+  head = document.getElementsByTagName('head')[0];
+  if (!head) { return; }
+  style = document.createElement('style');
+  style.type = 'text/css';
+  style.innerHTML = css;
+  head.appendChild(style);
+}
 
-    // Your code here...
-})();
+//addGlobalStyle('.switch input { opacity: 0; width: 0; height: 0; }'); Example of how to add css element
+
+$(document).ready(function() {
+   'use strict';
+   var historyApp = $('ul.nav.nav-pills.pull-left');
+   historyApp.append('<li><a class="btn btn-link ticketHistoryButton" title="My Ticket History"><span class="fa fa-history fa-nopad" aria-hidden="true"></span><span class="hidden-xs padding-left-xs">My Ticket History</span></a></li>');
+});
