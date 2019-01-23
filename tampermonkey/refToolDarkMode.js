dec56d20b10d8030092a1ccce5dc81a0aa218491
Tran <trankie@tss.oregonstate.edu>
2019-01-23 13:52:46 -0800

    Finished up with dark mode for reftool

--- tampermonkey/refToolDarkMode.js
+++ tampermonkey/refToolDarkMode.js
@@ -23,16 +23,49 @@ function addGlobalStyle(css) {
 
 function setDarkMode() {
     if (darkModeEnable === false){
-        console.log("You fucked up");
+        addGlobalStyle('body { background-color: #232323bf !important;}');
+        addGlobalStyle('div.panel.panel-default {background-color: #888888c2; border-color: #888888c2}');
+        addGlobalStyle('h4 {color: white}');
+        addGlobalStyle('div.col-md-12 {background-color: #ffffff75; border-color: #ffffff75}');
+        addGlobalStyle('div.container-fluid {background-color: #333333;}');
+        $("input.form-control").addClass('wrapper');
+        $("select.form-control.ng-pristine.ng-valid").addClass('wrapper');
         darkModeEnable = true;
     }else if(darkModeEnable === true){
-        console.log("You fucked up hard!!!");
+        addGlobalStyle('body { background-color: #EEEEEE !important;}');
+        addGlobalStyle('div.panel.panel-default {background-color: white; border-color: white}');
+        addGlobalStyle('h4 {color: black}');
+        addGlobalStyle('div.col-md-12 {background-color: white; border-color: white}');
+        addGlobalStyle('div.container-fluid {background-color: #008cba;}');
+        $("input.form-control").removeClass('wrapper');
+        $("select.form-control.ng-pristine.ng-valid").removeClass('wrapper');
         darkModeEnable = false;
     }
 }
 
 addGlobalStyle('.button { background-color: transparent; border: none; color: white; padding-top: 12px; padding-bottom: 12px; line-height: 21px; display: block; padding-right: 15px; padding-left: 15px;}');
+addGlobalStyle('.wrapper { background: linear-gradient(124deg, #ff2400, #e81d1d, #e8b71d, #e3e81d, #1de840, #1ddde8, #2b1de8, #dd00f3, #dd00f3);'
+    +'background-size: 1800% 1800%;'
+    +'-webkit-animation: rainbow 18s ease infinite;'
+    +'-z-animation: rainbow 18s ease infinite;'
+    +'-o-animation: rainbow 18s ease infinite;'
+    +'animation: rainbow 18s ease infinite;}');
 
+addGlobalStyle('@-webkit-keyframes rainbow { 0%{background-position:0% 82%}'
+      +'50%{background-position:100% 19%}'
+      +'100%{background-position:0% 82%}}');
+
+addGlobalStyle('@-moz-keyframes rainbow { 0%{background-position:0% 82%}'
+      +'50%{background-position:100% 19%}'
+      +'100%{background-position:0% 82%}}');
+      
+addGlobalStyle('@-o-keyframes rainbow { 0%{background-position:0% 82%}'
+      +'50%{background-position:100% 19%}'
+      +'100%{background-position:0% 82%}}');
+
+addGlobalStyle('@keyframes rainbow { 0%{background-position:0% 82%}'
+      +'50%{background-position:100% 19%}'
+      +'100%{background-position:0% 82%}}');
 
 $(document).ready(function() {
     'use strict';
